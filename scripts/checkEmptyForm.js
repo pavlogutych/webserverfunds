7b7f5ce7e5514430668fb403422d3cbba640d659
pavlogutych <54946146+pavlogutych@users.noreply.github.com>
2019-09-24 18:53:37 -0400

    updated javascript

---
+++ scripts/checkEmptyForm.js
@@ -0,0 +1,19 @@
+function checkEmptyForm() {
+
+    var first = document.forms["subs"]["first"].value;
+    var last = document.forms["subs"]["last"].value;
+    var email = document.forms["subs"]["email"].value;
+
+    if (first == "") {
+        alert("Please fill up your first name");
+    }
+    else if (last == "") {
+        alert("Please fill up your last name");
+    }
+    else if (email == "") {
+        alert("Please fill up your e-mail");
+    }
+    else {
+        alert("Thank you for submitting your inscription, we will contact you soon.")
+    }
+}
